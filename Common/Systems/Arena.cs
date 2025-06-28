d965cf9f6715100d8ea4f0fb31955809ee082af9
Garmon <Ljsm0007@gmail.com>
2025-06-28 10:16:16 -0500

    Add project files.

---
+++ Common/Systems/Arena.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PenguinPartyPort.Common.Systems;
+
+public class Arena
+{
+    public int Width { get; }
+    public int Height { get; }
+
+    public Arena(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+
+}
