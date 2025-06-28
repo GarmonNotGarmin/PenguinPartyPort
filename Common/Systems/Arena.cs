7380df33dc4aa5b6567d125e5cb25c5eb3ab6ca0
Garmon <Ljsm0007@gmail.com>
2025-06-28 11:04:57 -0500

    Incomplete Schematic and Arena files real

--- Common/Systems/Arena.cs
+++ Common/Systems/Arena.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,11 +7,13 @@ namespace PenguinPartyPort.Common.Systems;
 
 public class Arena
 {
+    public Vector2 Position { get; set; }
     public int Width { get; }
     public int Height { get; }
 
-    public Arena(int width, int height)
+    public Arena(Vector2 position, int width, int height)
     {
+        Position = position;
         Width = width;
         Height = height;
     }
---
+++ Common/Systems/Schematic.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace PenguinPartyPort.Common.Systems;
+
+public class Schematic
+{
+    [JsonProperty("Name")]
+    public string Name { get; }
+
+    public Schematic(string name)
+    {
+        Name = name;
+    }
+
+    public static Tile[]? ParseFile(string fileName)
+    {
+        string filePath = FindFilePath(fileName);
+        using (var stream = File.OpenRead(filePath))
+        {
+            
+        }
+
+        return null;
+    }
+
+    private static string FindFilePath(string fileName)
+    {
+        string filePath = "PenguinPartyPort/Schematics/" + fileName;
+        if (ModContent.FileExists(filePath))
+        {
+            return filePath;
+        }
+        else
+        {
+            throw new FileNotFoundException();
+        }
+        return "";
+    }
+}
